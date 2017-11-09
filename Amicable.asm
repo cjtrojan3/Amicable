3245d681c1f8554079c5614b449f95bec2471056
cjtrojan3 <31742052+cjtrojan3@users.noreply.github.com>
2017-11-09 10:26:26 -0600

    Done - Still slow as hell

--- Amicable.asm
+++ Amicable.asm
@@ -1,23 +1,37 @@
 .data
 	getLower: .asciiz "Input the start of the range: "
 	getUpper: .asciiz "Input the end of the range: "
-	swapValues: .asciiz "End of range < start of range -- swapping values"
+	swapValues: .asciiz "End of range < start of range -- swapping values\n"
+	areAmicable: .asciiz " are amicable numbers\n"
+	newLine: .asciiz "\n"
+	range: .asciiz "Range of numbers: "
+	dash: .asciiz "-----\n"
+	numPairs: .asciiz "Pairs of amicable numbers = "
+	dying: .asciiz "Unable to check non-positive values\nExiting ....."
 	
 .text
-	li $v0, 4		#Specify that we are printing a string
+	jal readIn
+	jal swap
+	
+readIn:	li $v0, 4		#Specify that we are printing a string
 	la $a0, getLower	#Load the string
 	syscall			
 	li $v0, 5		#Load the starting bound to $v0
 	syscall
 	move $s0, $v0		#Move starting bound to $s0
+	slt $t6, $zero, $s0	#If negative number, end program
+	beq $t6, $zero die
 	li $v0, 4		#Speify that we are printing a string
 	la $a0, getUpper	#Load the string
 	syscall			
 	li $v0, 5		#Load the ending bound to $v0
 	syscall
 	move $s1, $v0		#Move the ending bound to $s1
+	slt $t6, $zero, $s1	#If negative number, end program
+	beq $t6, $zero die
+	jr $ra
 	
-	slt $t0, $s0, $s1	#If lower bound is less than upper bound
+swap:	slt $t0, $s0, $s1	#If lower bound is less than upper bound
 	bne $t0, $zero, calc	#We can go to the calculations now
 	
 	li $v0, 4		#Specify that we are printing a string
@@ -27,37 +41,55 @@
 	xor $s0, $s0, $s1	#Quick swapping...
 	xor $s1, $s0, $s1	#...
 	xor $s0, $s0, $s1	#...
-	
-	
-calc:	#li $v0, 1
-	#add $a0, $s0, $zero
-	#syscall
-	
-	#li $v0, 1
-	#add $a0, $s1, $zero
-	#syscall
-	
-	sub $s2, $s1, $s0	#How many times we are looping
+
+calc:	sub $s2, $s1, $s0	#How many times we are looping
+	add $s5, $zero, $zero	#If we have founda a value before
+	add $s6, $zero, $zero	
+	add $s7, $s0, $zero
+	add $t5, $zero, $zero	#Count number of pairs
 
 loop:	beq $s2, $zero, exit	#If we reached our max value...
+	sub $s6, $s5, $s0	#See if we've done this before
+	beq $s6, $zero, adjVal	#If we have, go to the next number
 	add $a0, $s0 $zero	#Move our number to function argument
 	jal sumDiv		#Calculate its total divisors	
 	add $s3, $v0, $zero	#$s3 holds $s0's divisor sum
-	
+	beq $s3 $s0 adjVal
 	add $a0, $s3, $zero	#Move result sumDivisors($s0) to function argument
-	jal sumDiv
-	
+	jal sumDiv		#Calculate the sumDivisors of the original return value
 	add $s4, $v0 $zero	#$s4 holds the total of the divisors of $s0's sumDivisor total
 	
-	li $v0, 1
+	add $a0, $s0, $zero	#Moving values...
+	add $a1, $s4, $zero	#...
+	jal isAmic		#Test if the two are amicable
+	beq $v0 $zero printy	#If yes, print them out
+	
+	
+adjVal:	addi $s0, $s0, 1	#Add one to number to find sumDivisors of
+	addi $s2, $s2, -1	#Subtract one from our counter
+	j loop			#Loop again
+
+	
+printy:	add $s5, $s3, $zero	#$s5 is the bigger of the amicable pair
+	addi $t5, $t5, 1	#Add one to our total counter of how many pairs we found
+
+	li $v0, 1		#Lower number
+	move $a0, $s4
+	syscall
+
+	li $a0, ' '
+	la $v0, 11
+	syscall	
+
+	li $v0, 1		#Upper number
 	move $a0, $s3
 	syscall
 	
-	li $v0, 1
-	move $a0, $s4
+	li $v0, 4		#Specify that we are printing a string
+	la $a0, areAmicable	#Load the string
 	syscall
-	
-	j exit
+
+	j adjVal
 	
 sumDiv:	add $t0, $a0, $zero	#Move argument to temp address
 	addi $t1, $zero, 2	#$t1 = 2
@@ -79,6 +111,67 @@ addDat:	add $v0, $v0, $t3	#Add the functioning divisor to our running total
 donSum:	jr $ra			#Return - $v0 should have the total sum of divisors
 	
 	
+isAmic:	beq $a0 $a1 match	#If we have found a pair...
+	addi $v0, $zero, 1	#If not, set set, $v0 to 1
+	jr $ra			#Return
+	
+match:	add $v0, $zero, $zero	#Set $v0 to 0
+	jr $ra			#Return
 
-exit:	li $v0, 10		#Exit the program
+exit:	li $v0, 4
+	la $a0, newLine		#Skip a line
+	syscall	
+	
+	li $v0, 4
+	la $a0, range		#"Range of numbers..."
+	syscall	
+	
+	li $v0, 1
+	move $a0, $s7		#Start of the range
+	syscall
+	
+	li $a0, ' '
+	la $v0, 11
+	syscall	
+
+	li $a0, '-'
+	la $v0, 11
+	syscall	
+
+	li $a0, ' '
+	la $v0, 11
+	syscall	
+	
+	li $v0, 1
+	move $a0, $s1		#End of range
+	syscall
+	
+	li $v0, 4
+	la $a0, newLine		#New line
+	syscall	
+	
+	li $v0, 4
+	la $a0, numPairs	#"Pairs of amicable numbers..."
+	syscall	
+	
+	li $v0, 1
+	move $a0, $t5		#Number of amicable nubmers found
+	syscall
+	
+	li $v0, 4
+	la $a0, newLine		#New line
+	syscall	
+	
+	li $v0, 4
+	la $a0, dash		#Row of dashes
+	syscall	
+
+	li $v0, 10		#End program
+	syscall
+	
+die:	li $v0, 4		#Tell user they inputted a negative number
+	la $a0, dying
+	syscall	
+
+	li $v0, 10		#Die
 	syscall
